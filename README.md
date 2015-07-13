069898d51384d982af64abc1e9604c83b6ac86bb
Cory Bateman <splintercode.cb@gmail.com>
2015-07-13 00:45:16 -0500

    sln

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 A example repo for Vintage Software Training to intorduce Source Control with
 Git and Test Driven Development with JavaScript and JasmineJS.
+
+## Challenge
+
+Edit the validation.service.js `isValidPassword()` to check that passwords are at least 8 characters in length.
+Use the test password `Pass123`.
+Write a corresponding unit test for `isValidPassword()`.
---
+++ tests/lib/jasmine-2.3.4/result/result.js
@@ -0,0 +1,18 @@
+// func
+function isValidPassword(password) {
+    var containsDigits = /[0-9]/.test(password);
+    var containsUpper = /[A-Z]/.test(password);
+    var containsLower = /[a-z]/.test(password);
+    var containsAtLeast8Characters = /[a-zA-Z0-9]{8,}/;
+
+    if (containsDigits && containsUpper && containsLower && containsAtLeast8Characters) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// test
+it('Checks a invalid password "Pass123" is at least 8 characters in length is invalid', function() {
+    expect(validationService.isValidPassword('Pass123')).toBe(false);
+});
